122463aa1114ebd774b65125a2ef6fd7c45d7551
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-24 09:32:46 +1000

    Update mangrove.py

--- mangrove.py
+++ mangrove.py
@@ -7,16 +7,19 @@ class Mangrove:
     def __init__(self):
         self.variables = {}
         self.depths = {}
-        self.data = {}  # Store data values
-        self.structure_ordered = False  # Track whether structure is ordered
+        self.data = {}
+        self.structure_ordered = False
+        self.bundles = {}  # Store bundles as a dictionary
+        self.addresses = {}  # Store variable addresses
 
     def __setattr__(self, name, value):
-        if name in ["variables", "depths", "__class__", "data", "structure_ordered"]:
+        if name in ["variables", "depths", "__class__", "data", "structure_ordered", "bundles", "addresses"]:
             super().__setattr__(name, value)
         elif name in self.variables:
             raise ValueError(f"Variable {name} is already locked and cannot be reused.")
         else:
             self.variables[name] = value
+            self.addresses[name] = id(value)  # Store variable's memory address
             super().__setattr__(name, value)
 
     def __getattr__(self, name):
@@ -54,6 +57,7 @@ class Mangrove:
                             raise ValueError(f"Variable {var_name} is already locked and cannot be reused.")
                         else:
                             self.variables[var_name] = None
+                            self.addresses[var_name] = None
                             super().__setattr__(var_name, None)
                             if data_type not in self.depths[depth]["names"]:
                                 self.depths[depth]["names"][data_type] = []
@@ -61,24 +65,26 @@ class Mangrove:
 
     def bundle(self, cardinal=None, order=None):
         if cardinal is not None and order is not None:
+            bundle_dict = {}
             for depth, data_type in order.items():
                 depth = int(depth.split("depth")[-1])
                 if depth in self.depths:
                     if data_type in self.depths[depth]["types"]:
                         if len(self.depths[depth]["names"][data_type]) < int(cardinal):
-                            # Raise an error if cardinality is not met
                             raise ValueError(f"Cardinality number of {data_type} not met at depth {depth}.")
                     else:
                         raise ValueError(f"Data type {data_type} is not allowed at depth {depth}.")
                 else:
                     raise ValueError(f"Depth {depth} must be configured before using it.")
-            
-            self.structure_ordered = True  # Mark structure as ordered
+                
+                bundle_dict[f"bundle {depth}"] = data_type  # Store bundle info
+
+            self.bundles = bundle_dict
+            self.structure_ordered = True
 
     def pop(self, variable_name=None):
         if self.structure_ordered:
             if variable_name is None:
-                # Print the ordered structure
                 structure_str = ""
                 for depth, data in sorted(self.depths.items()):
                     for data_type, var_names in data["names"].items():
@@ -89,3 +95,10 @@ class Mangrove:
                     return self.data[variable_name]
                 else:
                     return f"No data found for variable {variable_name}."
+
+    def dict(self):
+        return self.bundles  # Return the bundle dictionary
+
+    def adrs(self):
+        return {f"{depth}: {var}": hex(self.addresses[var]) for var, depth in self.variables.items()}
+
