0cbd86ea3772214362552381ed32911a25c2210c
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-24 09:34:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-# main.py
-from mangrove import Mangrove as mgv  # Import the Mangrove class from mangrove.py
+# test_mangrove.py
+from mangrove import Mangrove  # Import the Mangrove class from mangrove.py
 
-# Detailed use case scenario
 def main():
-    x = mgv()
+    # Create an instance of Mangrove
+    experiment_data = Mangrove()
 
     # Configure depth and data types
-    x.config(depth=1, types=["tensor", "bool"], name=["tensor: tensor1, tensor2", "bool: bool1, bool2"])
-    x.config(depth=2, types=["int", "str"], name=["int: int1, int2", "str: str1, str2"])
+    experiment_data.config(
+        depth=1, 
+        types=["tensor", "bool"], 
+        name=["tensor: tensor1, tensor2", "bool: bool1, bool2"]
+    )
+    experiment_data.config(
+        depth=2, 
+        types=["int", "str"], 
+        name=["int: int1, int2", "str: str1, str2"]
+    )
 
     # Print configured data structure
-    x.pop()
+    experiment_data.pop()
 
     # Configure bundling and order
     order = {"depth1": "tensor", "depth2": "tensor", "depth1": "int"}
-    x.bundle(cardinal="2", order=order)
+    experiment_data.bundle(cardinal="2", order=order)
 
     # Access data using x.data.variable_name
-    print(x.data_tensor1_1)  # Access data using x.data.variable_name syntax
+    print(experiment_data.data_tensor1_1)  # Access data using x.data.variable_name syntax
+
+    # Print bundle dictionary
+    print(experiment_data.dict())
+
+    # Print variable memory addresses
+    print(experiment_data.adrs())
 
 if __name__ == "__main__":
     main()
