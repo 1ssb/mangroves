7e8f8b78652e7a2dede36f57ffd91d9cbcfc66fc
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-23 16:40:35 +1000

    Update mangrove.py

--- mangrove.py
+++ mangrove.py
@@ -4,3 +4,22 @@
 
 import numpy as np
 import dask as da
+
+class Mangrove:
+    def __init__(self):
+        self.variables = {}
+
+    def __setattr__(self, name, value):
+        if name == "variables":
+            super().__setattr__(name, value)
+        elif name in self.variables:
+            raise ValueError(f"Variable {name} is already locked and cannot be reused.")
+        else:
+            self.variables[name] = value
+            super().__setattr__(name, value)
+
+mgv = Mangrove()
+mgv.var1 = 10
+print(mgv.var1) # 10
+mgv.var1 = 20 # ValueError: Variable var1 is already locked and cannot be reused.
+
