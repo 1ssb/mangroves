0c290b3609e444b420c88c98535b6824987f4030
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-23 16:46:09 +1000

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 import mangrove as mgv
 
 x = mgv.Mangrove()
-x.var1 = 10
+x.config(var1=10, var2="hello", var3=[1, 2, 3], var4={"a": 1, "b": 2})
 print(x.var1) # 10
+print(x.var2) # hello
+print(x.var3) # [1, 2, 3]
+print(x.var4) # {'a': 1, 'b': 2}
 x.var1 = 20 # ValueError: Variable var1 is already locked and cannot be reused.
