2b7f1ac22f6c36632bcf75ab90b306f0d87a4f86
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-23 21:45:31 +1000

    Update code.py

--- code.py
+++ code.py
@@ -8,3 +8,6 @@ print(x.bool1) # None
 print(x.int1) # None
 print(x.str1) # None
 x.tensor1 = 10 # ValueError: Variable tensor1 is already locked and cannot be reused.
+x.pop()
+# Depth: 1: [type: tensor: variable_names:{tensor1, tensor2}]; [type: bool: variable_names:{bool1, bool2}];
+# Depth: 2: [type: int: variable_names:{int1, int2}]; [type: str: variable_names:{str1, str2}];
