02e072f63d39aaa9bc2b064e1e08f39ccd38ef1a
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-24 09:20:06 +1000

    Update mangrove.py

--- mangrove.py
+++ mangrove.py
@@ -5,14 +5,13 @@
 
 class Mangrove:
     def __init__(self):
-        # Initialize the attributes of the class
-        self.variables = {} 
-        self.depths = {} 
-        self.__class__ = "mangrove"
-        self.data = {}
+        self.variables = {}
+        self.depths = {}
+        self.data = {}  # Store data values
+        self.structure_ordered = False  # Track whether structure is ordered
 
     def __setattr__(self, name, value):
-        if name in ["variables", "depths", "__class__", "data"]:
+        if name in ["variables", "depths", "__class__", "data", "structure_ordered"]:
             super().__setattr__(name, value)
         elif name in self.variables:
             raise ValueError(f"Variable {name} is already locked and cannot be reused.")
@@ -21,6 +20,8 @@ class Mangrove:
             super().__setattr__(name, value)
 
     def __getattr__(self, name):
+        if name.startswith('data_') and name[5:] in self.data:
+            return self.data[name[5:]]
         if name in self.variables:
             return self.variables[name]
         else:
@@ -58,28 +59,33 @@ class Mangrove:
                                 self.depths[depth]["names"][data_type] = []
                             self.depths[depth]["names"][data_type].append(var_name)
 
-    def pop(self, variable_name=None):
-        if variable_name is None:
-            for depth, data in sorted(self.depths.items()):
-                print(f"Depth: {depth}: ", end="")
-                for data_type, var_names in data["names"].items():
-                    print(f"[type: {data_type}: variable_names:{{{', '.join(var_names)}}}]; ", end="")
-                print()
-        else:
-            if variable_name in self.data:
-                print(self.data[variable_name])
-            else:
-                print(f"No data found for variable {variable_name}.")
-    
     def bundle(self, cardinal=None, order=None):
         if cardinal is not None and order is not None:
             for depth, data_type in order.items():
                 depth = int(depth.split("depth")[-1])
                 if depth in self.depths:
                     if data_type in self.depths[depth]["types"]:
-                        if len(self.depths[depth]["names"][data_type]) != cardinal:
-                            raise ValueError(f"Cardinal number of {data_type} has not been configured in depth {depth}.")
+                        if len(self.depths[depth]["names"][data_type]) < int(cardinal):
+                            # Raise an error if cardinality is not met
+                            raise ValueError(f"Cardinality number of {data_type} not met at depth {depth}.")
                     else:
                         raise ValueError(f"Data type {data_type} is not allowed at depth {depth}.")
                 else:
                     raise ValueError(f"Depth {depth} must be configured before using it.")
+            
+            self.structure_ordered = True  # Mark structure as ordered
+
+    def pop(self, variable_name=None):
+        if self.structure_ordered:
+            if variable_name is None:
+                # Print the ordered structure
+                structure_str = ""
+                for depth, data in sorted(self.depths.items()):
+                    for data_type, var_names in data["names"].items():
+                        structure_str += ":".join(var_names) + ":"
+                print(structure_str[:-1])
+            else:
+                if variable_name in self.data:
+                    return self.data[variable_name]
+                else:
+                    return f"No data found for variable {variable_name}."
