2d99a70b83d7709f34cda7f61c2094bea99e310a
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-26 20:47:14 +1000

    Update setup.py
    Signed-off-by: Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'listings',
+        'torch>=1.8.1',
+    ],
+    author='Subhransu S. Bhattacharjee',
+    author_email='your.email@example.com',
+    description='A Dynamic Data Management Engine for Advanced AI Applications',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/1ssb/mangroves',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
 )
