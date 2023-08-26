abb6278338e93eeeeb25fa9fcded015a70730e15
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-26 20:46:41 +1000

    Update __init__.py
    Signed-off-by: Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>

--- mangroves/__init__.py
+++ mangroves/__init__.py
@@ -1,5 +1,2 @@
-try:
-    from .mangrove import Mangrove, MangroveException
-except Exception as e:
-    raise ImportError(f"Failed to import from mangrove module: {e}")
+from .mangrove import Mangrove, MangroveException
 
