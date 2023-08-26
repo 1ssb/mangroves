1d29c921abf3b7e967dd4bf510adc722c3060112
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-08-26 17:20:47 +1000

    Update main.py
    Signed-off-by: Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>

--- main.py
+++ main.py
@@ -1,61 +1,28 @@
 # Code by 1ssb
+# Initialize Mangrove
 
-# test_mangrove.py
-from mangrove import Mangrove  # Import the Mangrove class from mangrove.py
 
-def main():
-    mangrove = Mangrove()
-    
-    # Configure the allowed types for each depth
-    mangrove.config(depth=1, types=[int])
-    mangrove.config(depth=2, types=[list])
-    mangrove.config(depth=3, types=[dict])
-    
-    # Add data to the Mangrove instance
-    mangrove.add_data(depth=1, type=int, var=["x"], value=1)
-    mangrove.add_data(depth=2, type=list, var=["y"], value=[1, 2])
-    mangrove.add_data(depth=3, type=dict, var=["z"], value={"a": 1})
+m = Mangrove()
 
-    # Access and modify the data attributes
-    print(mangrove.x) # 1
-    mangrove.x = 2
-    print(mangrove.x) # 2
-    
-    # Get a summary of the data stored in the tree
-    summary = mangrove.summary()
-    print(summary) # {'x': {'type': 'int', 'depth': 1}, 'y': {'type': 'list', 'depth': 2}, 'z': {'type': 'dict', 'depth': 3}}
-    
-    # Create another Mangrove instance
-    mangrove_instance = Mangrove()
-    mangrove_instance.config(depth=1, types=[int])
-    mangrove_instance.add_data(depth=1, type=int, var=["g"])
-    
-    # Access and modify the data attribute
-    print(mangrove_instance.g) # None
-    mangrove_instance.g = 1
-    print(mangrove_instance.g) # 1
+# Configure the allowed types for each depth
+m.config(depth=1, types=[int, torch.Tensor])
+m.config(depth=2, types=[torch.Tensor])
 
-    # Create an instance of the Mangrove class
-    instance = Mangrove()
+# Add data to the Mangrove instance
+m.add_data(depth=1, data_type=int, var=["x", "y", "z", "w", "v"], value=[1, 2, 3, 4, 5])
+m.add_data(depth=1, data_type=torch.Tensor, var=["a", "b", "c", "d", "e"], value=[torch.rand(3, 3) for _ in range(5)])
+m.add_data(depth=2, data_type=torch.Tensor, var=["f", "g", "h", "i", "j"], value=[torch.rand(3, 3) for _ in range(5)])
 
-    # Configure the allowed types for each depth
-    instance.config(depth=1, types=[int, str])
-    instance.config(depth=2, types=[int, str])
+# Fetching a value from Mangrove instance
+print("m.f: ", m.f)
 
-    # Add multiple variables
-    instance.add_data(depth=1, type=int, var=["x", "y", "z"])
-    instance.add_data(depth=1, type=int, var=["a", "b", "c"])
-    instance.add_data(depth=2, type=str, var=["var"])
+# Summary
+print("Summary: ", m.summary())
 
-    # Update the existing variables
-    instance.x = 10
+# Index: Fetch variables with certain depths or types
+print("Index with depth=1 and type=int: ", m.index(depth=1, data_type=int))
+print("Index with type=torch.Tensor: ", m.index(data_type=torch.Tensor))
 
-    # Print the variables for specified depth and data type
-    print("Variables at Depth 1 and int Data Type:")
-    print(instance.index(depth=1, data_type=int))
-
-    print("Variables at Depth 2 and str Data Type:")
-    print(instance.index(depth=2, data_type=str))
-
-if __name__ == "__main__":
-    main()
+# Var: Get variable names with certain depths or types
+print("Var with depth=1: ", m.var(depth=1))
+print("Var with type=torch.Tensor: ", m.var(data_type=torch.Tensor))
